0f9d1c3b9eaab67a718443da0e9bba5ce21ef359
anuzzzzz <anuj.jokhani@gmail.com>
2025-11-26 12:58:55 -0700

    adding databases and validation logic

--- health/admin.py
+++ health/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Prescription, Event
 
 # Register your models here.
+
+@admin.register(Prescription)
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ['patient_name','medication_name','status','updated_at']
+    list_filter = ['status']
+    search_fields = ['patient_name', 'medication_name']
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['prescription','event_type','performed_by','created_at']
+    list_filter = ['event_type']
+
